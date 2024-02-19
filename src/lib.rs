74275fee46bdd7b87fcc96a8de76ca940362c2d5
Maarten van Gompel <proycon@anaproy.nl>
2024-02-19 20:34:07 +0100

    forgot to commit this after moving files

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+mod annotate;
+mod grep;
+mod info;
+mod query;
+mod tag;
+mod to_text;
+mod tsv;
+mod validate;
+mod view;
+
+pub use crate::annotate::*;
+pub use crate::grep::*;
+pub use crate::info::*;
+pub use crate::query::*;
+pub use crate::tag::*;
+pub use crate::to_text::*;
+pub use crate::tsv::*;
+pub use crate::validate::*;
+pub use crate::view::*;
+
+pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
